048db5e2140df51c74473d7f8955860703cd739d
Lupascu Marian <marianlupascu15@gmail.com>
2018-12-13 00:03:05 +0200

    Add authentication details :oncoming_automobile:

--- JiraCloneMVC.Web/Controllers/ProjectsController.cs
+++ JiraCloneMVC.Web/Controllers/ProjectsController.cs
@@ -1,38 +1,32 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Data;
-using System.Data.Entity;
+﻿using System.Data.Entity;
 using System.Linq;
 using System.Net;
-using System.Web;
 using System.Web.Mvc;
-using JiraCloneMVC.Web;
 using JiraCloneMVC.Web.Models;
+using Microsoft.AspNet.Identity;
 
 namespace JiraCloneMVC.Web.Controllers
 {
     public class ProjectsController : Controller
     {
-        private ApplicationDbContext db = new ApplicationDbContext();
+        private readonly ApplicationDbContext db = new ApplicationDbContext();
 
         // GET: Projects
         public ActionResult Index()
         {
+            if (TempData.ContainsKey("message")) ViewBag.message = TempData["message"].ToString();
+
             return View(db.Projects.ToList());
         }
 
         // GET: Projects/Details/5
         public ActionResult Details(int? id)
         {
-            if (id == null)
-            {
-                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
-            }
-            Project project = db.Projects.Find(id);
-            if (project == null)
-            {
-                return HttpNotFound();
-            }
+            if (id == null) return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+
+            var project = db.Projects.Find(id);
+            if (project == null) return HttpNotFound();
+
             return View(project);
         }
 
@@ -47,10 +41,14 @@ namespace JiraCloneMVC.Web.Controllers
         // more details see https://go.microsoft.com/fwlink/?LinkId=317598.
         [HttpPost]
         [ValidateAntiForgeryToken]
-        public ActionResult Create([Bind(Include = "Id,Name,Description,Status,StartDate,EndDate")] Project project)
+        public ActionResult Create([Bind(Include = "Id,Name,Description,Status,StartDate,EndDate")]
+            Project project)
         {
             if (ModelState.IsValid)
             {
+                project.OrganizerId = User.Identity.GetUserId();
+                //when create a new proj must become organizer - TO DO THIS !!!!!!!!!!!!!!!!!!!!
+
                 db.Projects.Add(project);
                 db.SaveChanges();
                 return RedirectToAction("Index");
@@ -60,18 +58,21 @@ namespace JiraCloneMVC.Web.Controllers
         }
 
         // GET: Projects/Edit/5
+        //[Authorize(Roles = "Organizator,Administrator")]
         public ActionResult Edit(int? id)
         {
-            if (id == null)
-            {
-                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
-            }
-            Project project = db.Projects.Find(id);
-            if (project == null)
-            {
-                return HttpNotFound();
-            }
-            return View(project);
+            if (id == null) return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+
+            var project = db.Projects.Find(id);
+
+            if (project == null) return HttpNotFound();
+
+            if (project.OrganizerId == User.Identity.GetUserId() || User.IsInRole("Administrator"))
+                return View(project);
+
+            TempData["message"] = "You do not have the right to make changes to " +
+                                  "a PROJECT that does not belong to you!";
+            return RedirectToAction("Index");
         }
 
         // POST: Projects/Edit/5
@@ -79,49 +80,68 @@ namespace JiraCloneMVC.Web.Controllers
         // more details see https://go.microsoft.com/fwlink/?LinkId=317598.
         [HttpPost]
         [ValidateAntiForgeryToken]
-        public ActionResult Edit([Bind(Include = "Id,Name,Description,Status,StartDate,EndDate")] Project project)
+        //[Authorize(Roles = "Organizator,Administrator")]
+        public ActionResult Edit([Bind(Include = "Id,Name,Description,Status,StartDate,EndDate")]
+            Project project)
         {
             if (ModelState.IsValid)
             {
-                db.Entry(project).State = EntityState.Modified;
-                db.SaveChanges();
+                if (project.OrganizerId == User.Identity.GetUserId() || User.IsInRole("Administrator"))
+                {
+                    db.Entry(project).State = EntityState.Modified;
+                    db.SaveChanges();
+                    return RedirectToAction("Index");
+                }
+
+                TempData["message"] = "You do not have the right to make changes to " +
+                                      "a PROJECT that does not belong to you!";
                 return RedirectToAction("Index");
             }
+
             return View(project);
         }
 
         // GET: Projects/Delete/5
+        //[Authorize(Roles = "Organizator,Administrator")]
         public ActionResult Delete(int? id)
         {
-            if (id == null)
-            {
-                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
-            }
-            Project project = db.Projects.Find(id);
-            if (project == null)
-            {
-                return HttpNotFound();
-            }
-            return View(project);
+            if (id == null) return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+
+            var project = db.Projects.Find(id);
+            if (project == null) return HttpNotFound();
+
+            if (project.OrganizerId == User.Identity.GetUserId() || User.IsInRole("Administrator"))
+                return View(project);
+
+            TempData["message"] = "You do not have the right to make changes to " +
+                                  "a PROJECT that does not belong to you!";
+            return RedirectToAction("Index");
         }
 
         // POST: Projects/Delete/5
-        [HttpPost, ActionName("Delete")]
+        [HttpPost]
+        [ActionName("Delete")]
         [ValidateAntiForgeryToken]
+        //[Authorize(Roles = "Organizator,Administrator")]
         public ActionResult DeleteConfirmed(int id)
         {
-            Project project = db.Projects.Find(id);
-            db.Projects.Remove(project);
-            db.SaveChanges();
+            var project = db.Projects.Find(id);
+            if (project.OrganizerId == User.Identity.GetUserId() || User.IsInRole("Administrator"))
+            {
+                db.Projects.Remove(project);
+                db.SaveChanges();
+                return RedirectToAction("Index");
+            }
+
+            TempData["message"] = "You do not have the right to make changes to " +
+                                  "a PROJECT that does not belong to you!";
             return RedirectToAction("Index");
         }
 
         protected override void Dispose(bool disposing)
         {
-            if (disposing)
-            {
-                db.Dispose();
-            }
+            if (disposing) db.Dispose();
+
             base.Dispose(disposing);
         }
     }
--- JiraCloneMVC.Web/Models/Project.cs
+++ JiraCloneMVC.Web/Models/Project.cs
@@ -13,7 +13,7 @@ namespace JiraCloneMVC.Web.Models
         public String Status { get; set; }
         public DateTime StartDate { get; set; }
         public DateTime EndDate { get; set; }
-
+        public String OrganizerId { get; set; }
         public User Organizer { get; set; }
 
         public ICollection<Group> Groups { get; set; }
