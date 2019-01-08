4dc132b4936b2ca7c292c9c01cd9046f6e317571
Lupascu Marian <marianlupascu15@gmail.com>
2019-01-08 23:13:48 +0200

    Add frontend

--- JiraCloneMVC.Web/Constants.cs
+++ JiraCloneMVC.Web/Constants.cs
@@ -8,5 +8,11 @@
             public const string InProgress = "In Progress";
             public const string Done = "Done";
         }
+
+        public class ProjectStatus
+        {
+            public const string Closed = "Closed";
+            public const string Open = "Open";
+        }
     }
 }
